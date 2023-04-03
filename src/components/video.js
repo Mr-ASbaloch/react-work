cf5cc5647ab0b96a588e12ddb4bb234962fd2e02
abdul <abdulsaeed1356@gmail.com>
2023-04-03 10:29:40 +0500

    added

--- src/components/video.js
+++ src/components/video.js
@@ -5,7 +5,12 @@ function Video()
             <h1>video</h1>
            <p>heloo</p>
          <img src="./components/download (11).jpeg"  alt="img"/>
-           
+
+         <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
         </div>
 
     )
