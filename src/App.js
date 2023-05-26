e5bba9ad368d1912277c14fc33145cc6653388d4
abdul <abdulsaeed1356@gmail.com>
2023-05-26 16:17:47 +0500

    solved

--- src/App.js
+++ src/App.js
@@ -1,47 +1,46 @@
 import Header from "./components/Header";
 import "./App.css";
-// import Movie from "./components/Movie";
-// import Movies from "./Movie.json";
-// import UseState from "./components/useState";
-// import New from "./components/Record";
-import Contact from "./components/contact";
-import About from "./components/about";
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
+import Movie from "./components/Movie";
+import Movies from "./Movie.json";
+import UseState from "./components/useState";
+import New from "./components/Record";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import About from "./components/About";
+// import { Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <RouterProvider>
-      <Header></Header>
+      <Router>
+        <Routes>
+          
+        <Route path="/Header" element={<Header></Header>} ></Route> 
+        <Route path="/About" element={<About></About>} ></Route> 
+        <Route path="/Contact" element={<Contact></Contact>} ></Route> 
+        <Route path="/Home" element={<Home></Home>} ></Route> 
+          
+        </Routes>
 
-      {/* <div className="Main">
-        {Movies.map((Element, index) => {
-          return (
-            <Movie
-              key={index}
-              title={Element.Title}
-              img={Element.Poster}
-              runTime={Element.Runtime}
-              year={Element.Year}
-            ></Movie>
-          );
-        })}
-        <UseState></UseState>
        
-      </div>
-      <New></New>
-       */}
-        
-       
-       
-
-       </RouterProvider>  
-     
+      </Router>
+      <div className="Main">
+          {Movies.map((Element, index) => {
+            return (
+              <Movie
+                key={index}
+                title={Element.Title}
+                img={Element.Poster}
+                runTime={Element.Runtime}
+                year={Element.Year}
+              ></Movie>
+            );
+          })}
+         
+        </div>
+        <New></New>
+      <UseState></UseState>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 import React from 'react'
-
+import { Link} from 'react-router-dom'
 export default function Header() {
   return (
     <>
         <div style={{backgroundColor:'aquamarine' ,height:70  }} >
       <h1 style={ {textAlign:'center' ,margin:0 }}>Movies Reviews</h1>
+    <Link to={'/About'}> 
+      <h1>About</h1>
+    </Link>
+    <Link to={'/About'}> 
+      <h1>Home</h1>
+    </Link>
+    <Link to={'/About'}> 
+      <h1>Contact</h1>
+    </Link>
     </div>
+  
+    
     </>
   )
 }
