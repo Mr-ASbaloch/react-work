ef9bdea161f7135f2f9ba9215e79abd9e7591dc1
abdul <abdulsaeed1356@gmail.com>
2023-05-27 10:18:48 +0500

    solved

--- src/App.js
+++ src/App.js
@@ -1,46 +1,72 @@
 import Header from "./components/Header";
 import "./App.css";
-import Movie from "./components/Movie";
-import Movies from "./Movie.json";
-import UseState from "./components/useState";
+// import Movie from "./components/Movie";
+// import Movies from "./Movie.json";
+// import UseState from "./components/useState";
 import New from "./components/Record";
-import Home from "./components/Home";
-import Contact from "./components/Contact";
-import About from "./components/About";
-// import { Router } from "react-router-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Brightness3TwoTone } from "@mui/icons-material";
+// import { Key } from "@mui/icons-material";
 
 function App() {
+  const [state, setState] = useState(2);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const get= await fetch(
+        `https://hub.dummyapis.com/employee?noofRecords=${state}&idStarts=1001`
+      );
+      const res = await get.json();
+      setData(res);
+      console.log(res);
+    
+    }
+    getData();
+    document.title=`( ${state}) employees available`;
+  }, [state]);
+
   return (
     <>
-      <Router>
-        <Routes>
-          
-        <Route path="/Header" element={<Header></Header>} ></Route> 
-        <Route path="/About" element={<About></About>} ></Route> 
-        <Route path="/Contact" element={<Contact></Contact>} ></Route> 
-        <Route path="/Home" element={<Home></Home>} ></Route> 
-          
-        </Routes>
+      <Header></Header>
 
-       
-      </Router>
-      <div className="Main">
-          {Movies.map((Element, index) => {
-            return (
-              <Movie
-                key={index}
-                title={Element.Title}
-                img={Element.Poster}
-                runTime={Element.Runtime}
-                year={Element.Year}
-              ></Movie>
-            );
-          })}
-         
-        </div>
-        <New></New>
-      <UseState></UseState>
+      <div className="fetch">
+        <button  onClick={() => setState(state + 2)}>{state} </button>
+      </div>
+      {/* <div className="Main">
+        {Movies.map((Element, index) => {
+          return (
+            <Movie
+              key={index}
+              title={Element.Title}
+              img={Element.Poster}
+              runTime={Element.Runtime}
+              year={Element.Year}
+            ></Movie>
+          );
+        })} 
+
+        <UseState></UseState>
+      </div> */}
+
+      {
+        data.map ((Element, index) => {
+          return (
+            <div key={index} className="data">
+              <h1>{Element.firstName}</h1>
+              <h4>{Element.lastName}</h4>
+              <h4>{Element.email}</h4>
+              <h4>{Element.age}</h4>
+              <h4>{Element.contactNumber}</h4>
+              
+             
+            </div>
+          );
+        })
+      }
+
+      
+      {/* <New></New> */}
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,12 @@
 import React from 'react'
-import { Link} from 'react-router-dom'
-export default function Header() {
+
+const Header = () => {
   return (
-    <>
-        <div style={{backgroundColor:'aquamarine' ,height:70  }} >
-      <h1 style={ {textAlign:'center' ,margin:0 }}>Movies Reviews</h1>
-    <Link to={'/About'}> 
-      <h1>About</h1>
-    </Link>
-    <Link to={'/About'}> 
-      <h1>Home</h1>
-    </Link>
-    <Link to={'/About'}> 
-      <h1>Contact</h1>
-    </Link>
-    </div>
-  
     
-    </>
+       <div style={{backgroundColor:'aquamarine' ,height:70  }} >
+      <h1 style={ {textAlign:'center' ,margin:0 }}>Employee Data</h1>
+    </div>
   )
 }
+
+export default Header
